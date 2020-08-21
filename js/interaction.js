05944e4e5916e89d04b386af7c059d9c6551d156
Hyeon Seong LEE <dynamic1117@gmail.com>
2020-08-22 00:48:36 +0900

    semi-final
    semi-final

--- js/interaction.js
+++ js/interaction.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    slider_num=1;
     
     $('header').hover(function() {
         $('nav').toggleClass('open');
@@ -15,5 +14,16 @@ $(document).ready(function() {
             $("header > div").removeClass('scroll');
         }
     })
+    var n=0;
+    
+    setInterval(function(){
+        n++;
+        if(n==4){
+            n=0;
+        }
+        $('.mockup_wrap').delay(2000).animate({
+            left:-520*n
+        },1000);
+    },3000)
     
 });
