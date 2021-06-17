7eed4fb37579d3ae4b32013612a87fb80aca4a3a
Hyeon Seong LEE <dynamic1117@gmail.com>
2021-06-18 01:11:04 +0900

    ui improvement completed
    ui improvement completed

--- js/interaction.js
+++ js/interaction.js
@@ -42,90 +42,18 @@ $(document).ready(function () {
     });
   });
 
-  var $slider_wrap = $(".slider_wrap");
-  /*
-  슬라이더 전면 재설계 필요
-  $(".next").click(function () {
-    $slider_wrap.animate(
-      {
-        opacity: 0,
-      },
-      500
-    );
-    slider_n++;
-    if (slider_n == 3) {
-      slider_n = 0;
+  var $slide_container = $(".slide_container");
+  var slides = $slide_container.children().length - 1;
+  var now = 0;
+  setInterval(function () {
+    now++;
+    if (now > slides) {
+      now = 0;
     }
-    $(".banner_wrap").animate(
-      {
-        left: -win_w * slider_n,
-      },
-      500,
-      function () {
-        if (slider_n == 1) {
-          $slider_wrap
-            .find("h2")
-            .html("다양한<p>게임을</p><span>선보입니다</span>");
-          $(".slider_wrap >p").html(
-            "우리의 목적은 단순한 이윤추구가 아닙니다. <span>성장성이 있는 인디 개발사의 안정적 시장 정착 또한 우리의 목표이기 때문이죠.</span>"
-          );
-          $slider_wrap
-            .animate(
-              {
-                opacity: 1,
-              },
-              500
-            )
-            .css({
-              color: "#fff",
-            });
-          $slider_wrap
-            .find(".btn")
-            .css({
-              color: "#fff",
-              border: "1px solid #fff",
-            })
-            .text("전체 사업");
-        } else if (slider_n == 2) {
-          $slider_wrap.find("h2").html("One Step to<span>New World</span>");
-          $(".slider_wrap > p").html(
-            "게임을 켤 때마다 <span>새로운 세상으로 한 발자국 내딛는 듯한</span><span>새로움을 느끼실 수 있도록 노력하겠습니다</span>"
-          );
-          $slider_wrap
-            .animate(
-              {
-                opacity: 1,
-              },
-              500
-            )
-            .css({
-              color: "#000",
-            });
-          $(".slider_wrap .btn")
-            .css({
-              border: "1px solid #2a2b38",
-              color: "#2a2b38",
-            })
-            .text("전체 사업");
-        } else {
-          $slider_wrap
-            .find("h2")
-            .html("Global<p>User-Friendly</p><span>Game Company</span>");
-          $(".slider_wrap > p").html(
-            "카카오게임즈는 자체 개발 역량 강화를 통해 <span>유저 친화적 게임 경험을 제공하기 위해 노력해왔습니다.</span>"
-          );
-          $slider_wrap.animate(
-            {
-              opacity: 1,
-            },
-            500
-          );
-          $slider_wrap.find(".btn").text("전체 서비스");
-        }
-      }
-    );
-  });
-  */
+    $slide_container.css({
+      transform: "translateX(-" + now * 100 + "vw)",
+    });
+  }, 3000);
 
   var n = 0;
   setInterval(function () {
@@ -133,13 +61,11 @@ $(document).ready(function () {
     if (n == 4) {
       n = 0;
     }
-    $(".mockup_wrap")
-      .delay(2000)
-      .animate(
-        {
-          left: -520 * n,
-        },
-        1000
-      );
+    $(".mockup_wrap").animate(
+      {
+        left: -520 * n,
+      },
+      1000
+    );
   }, 3000);
 });
